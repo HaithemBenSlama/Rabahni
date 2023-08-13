dd8fc3a9a0d68d4821c9952c83e0063d670dd942
HaithemBenSlama <haithem.benslama@hotmail.fr>
2023-08-13 03:40:38 +0100

    fixing some styles

--- components/Step.js
+++ components/Step.js
@@ -191,7 +191,7 @@ const Step = () => {
           </div>
           {message === 0 ? (
             <div
-              class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800"
+              class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 "
               role="alert"
             >
               <svg
@@ -211,7 +211,7 @@ const Step = () => {
             </div>
           ) : message === 1 ? (
             <div
-              class="flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800"
+              class="flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50"
               role="alert"
             >
               <svg
@@ -231,7 +231,7 @@ const Step = () => {
             </div>
           ) : message === 2 ? (
             <div
-              class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+              class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50"
               role="alert"
             >
               <svg
